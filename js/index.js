c40f99685effa5cd3090f6aaf94e10166abaec18
FreedTapstry21 <bojordans321@gmail.com>
2022-11-23 23:00:32 +0100

    Moved mode switcher to seperate file

--- js/index.js
+++ js/index.js
@@ -12,30 +12,6 @@
 console.log("Hello world!");
 
 /*
-    Freeddev Dark/Light mode switcher
-*/
-
-if (window.localStorage.getItem("view-mode") == null) {window.localStorage.setItem("view-mode", "Light mode");}
-if (window.localStorage.getItem("view-mode-class") == null) {window.localStorage.setItem("view-mode-class", "wallpaper dark");}
-
-document.getElementById("body").className = window.localStorage.getItem("view-mode-class");
-document.getElementById("view-switch").innerText = window.localStorage.getItem("view-mode");
-
-
-document.getElementById("view-switch").onclick = function () {
-    if (document.getElementById("view-switch").innerText == "Dark mode") {
-        window.localStorage.setItem("view-mode", "Light mode");
-        window.localStorage.setItem("view-mode-class", "wallpaper dark");
-        window.location = window.location;
-    }
-    if (document.getElementById("view-switch").innerText == "Light mode") {
-        window.localStorage.setItem("view-mode", "Dark mode");
-        window.localStorage.setItem("view-mode-class", "wallpaper light");
-        window.location = window.location;
-    }
-}
-
-/*
     Responsive Elements
 */
 
